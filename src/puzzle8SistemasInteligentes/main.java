5c1cd15f53eb2834f09e87908f5506f3b00d77ab
Thiago Mohr da Silveira <thiagomohrs@gmail.com>
2016-08-29 12:21:43 -0300

    trabalhando com arrayList

--- src/puzzle8SistemasInteligentes/main.java
+++ src/puzzle8SistemasInteligentes/main.java
@@ -30,20 +30,23 @@ public class main {
 		return matriz;
 	}
 
-	public static void calculaDistancia(int[][] matriz, int objetivo[][]) {
+	public static void calculaDistancia(int[][] matriz, int objetivo[][], List<Integer> distancia) {
 		int disttotal = 0;
+		int distvalor = 0;
 		for (int linhaob = 0; linhaob < 3; linhaob++) {
 			for (int colunaob = 0; colunaob < 3; colunaob++) {
 				int valorob = objetivo[linhaob][colunaob];
 				for (int linhama = 0; linhama < 3; linhama++) {
 					for (int colunama = 0; colunama < 3; colunama++) {
 						int valorma = matriz[linhama][colunama];
-						int distvalor = 0;
 						if (valorob == valorma) {
+							distvalor = 0;
 							int distcol = colunaob - colunama;
 							int distlin = linhaob - linhama;
 							distvalor = Math.abs(distcol) + Math.abs(distlin);
 							// imprime a distancia para cada peça(numero)
+							distancia.add(valorob, distvalor);
+							// distancia.set(valorob, distvalor);
 							System.out.println("Peça nº: " + valorob + " | Distancia de posição objetivo: " + distvalor);
 						}
 						disttotal += distvalor;
@@ -71,6 +74,12 @@ public class main {
 		return matriz;
 	}
 
+	public static int verificarSePosicaoObjetivoEstaProxima(int objetivo[][], int[][] matriz, int peca) {
+		int valor = 0;
+
+		return valor;
+	}
+
 	public static void main(String[] args) {
 
 		// matriz objetivo
@@ -78,6 +87,12 @@ public class main {
 
 		// matriz criada
 		int matriz[][] = new int[3][3];
+
+		// ArrayList para salvar a peça e sua distancia
+		// index é o valor da peça
+		List<Integer> distancia = new ArrayList<>();
+		// inicia index 0 com dist -1
+		distancia.add(-1);
 
 		// pede para usuario criar matriz
 		// criarMatriz(matriz);
@@ -88,7 +103,17 @@ public class main {
 		// imprime matriz criada
 		imprimir(matriz);
 
-		calculaDistancia(matriz, objetivo);
+		calculaDistancia(matriz, objetivo, distancia);
+
+		System.out.println(distancia);
+		imprimirPeloArrayList(distancia);
+
+	}
+
+	private static void imprimirPeloArrayList(List<Integer> distancia) {
+		for (int i = 1; i < distancia.size(); i++) {
+			System.out.println("Peça: " + i + " | Distancia: " + distancia.get(i));
+		}
 
 	}
 
