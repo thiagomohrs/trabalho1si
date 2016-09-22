9fadd7aa7ab72a0f035fcb7da591c692688215a8
Thiago Mohr <thiagomohrs@gmail.com>
2016-09-22 19:43:26 -0300

    revisão entrega

--- src/trabalho1si/Main.java
+++ src/trabalho1si/Main.java
@@ -5,8 +5,5 @@ public class Main {
 	public static void main(String[] args) {
 		Puzzle puzzle = new Puzzle();
 		puzzle.resolverJogo();
-
-//		Window m = new Window();
-//		m.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 }
